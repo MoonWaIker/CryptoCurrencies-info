7ddebddc7b5da3efde01f94f4d4f54222d62d2b9
MoonWalker <muhinmihajlo40@gmail.com>
2023-04-06 16:34:06 +0300

    Add new coin classes

---
+++ Cryptocurrencies info/Models/CryptoCurrencies/Coin.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+public class Coin
+{
+    [JsonProperty("market_cap_rank")]
+    public int Rank { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("current_price")]
+    public decimal CurrentPrice { get; set; }
+
+    [JsonProperty("price_change_percentage_24h")]
+    public decimal PriceChangePercentage24h { get; set; }
+
+    [JsonProperty("total_volume")]
+    public decimal Volume { get; set; }
+}
---
+++ Cryptocurrencies info/Models/CryptoCurrencies/CoinMarket.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+class CoinMarket
+{
+    private RestClient client = new RestClient("https://api.coingecko.com/api/v3");
+
+    // Get all coins
+    public Coin[]? GetCoinMarket() => GetCoinMarket(100);
+    public Coin[]? GetCoinMarket(int pages)
+    {
+        var response = client.Execute(new RestRequest("/coins/markets", Method.Get).AddParameter("vs_currency", "usd").AddParameter("per_page", pages));
+        return response.IsSuccessful ? JsonConvert.DeserializeObject<Coin[]>(response.Content) : null;
+    }
+
+    // Get the coin
+    public CoinFull? GetCoin(string CoinId)
+    {
+        var response = client.Execute(new RestRequest($"/coins/{CoinId}", Method.Get));
+        return response.IsSuccessful ? JsonConvert.DeserializeObject<CoinFull>(response.Content) : null;
+    }
+
+    // Exchange coins
+    public decimal? GetExchange(string TransferFrom, string TransferTo, decimal count)
+    {
+        var response = client.Execute(new RestRequest("/coins/markets", Method.Get).AddParameter("vs_currency", "usd").AddParameter("ids", $"{TransferFrom}, {TransferTo}"));
+        return response.IsSuccessful ? JArray.Parse(response.Content).Select(i => (decimal)i["current_price"]).Aggregate((x, y) => x / y * count) : null;
+    }
+
+    // Get the array of coins
+    public string[]? GetCoinArray()
+    {
+        var response = client.Execute(new RestRequest("/coins/list", Method.Get));
+        return response.IsSuccessful ? JArray.Parse(response.Content).Select(c => (string)c["id"]).ToArray() : null;
+    }
+}
