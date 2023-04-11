8a1077abf61b08a8a2c9cd9380fd0241ec5b859d
MoonWalker <muhinmihajlo40@gmail.com>
2023-04-11 15:36:00 +0300

    Changed API

--- Cryptocurrencies info/Models/CryptoCurrencies/Coin.cs
+++ Cryptocurrencies info/Models/CryptoCurrencies/Coin.cs
@@ -2,21 +2,21 @@
 
 public class Coin
 {
-    [JsonProperty("market_cap_rank")]
+    [JsonProperty("rank")]
     public int Rank { get; set; }
 
     [JsonProperty("name")]
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
 
     [JsonProperty("id")]
-    public string Id { get; set; }
+    public string Id { get; set; } = "";
 
-    [JsonProperty("current_price")]
+    [JsonProperty("priceUsd")]
     public decimal CurrentPrice { get; set; }
 
-    [JsonProperty("price_change_percentage_24h")]
+    [JsonProperty("changePercent24Hr")]
     public decimal PriceChangePercentage24h { get; set; }
 
-    [JsonProperty("total_volume")]
+    [JsonProperty("volumeUsd24Hr")]
     public decimal Volume { get; set; }
 }
--- Cryptocurrencies info/Models/CryptoCurrencies/CoinMarket.cs
+++ Cryptocurrencies info/Models/CryptoCurrencies/CoinMarket.cs
@@ -1,37 +1,43 @@
 ﻿using RestSharp;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 class CoinMarket
 {
-    private RestClient client = new RestClient("https://api.coingecko.com/api/v3");
+    private RestClient client = new RestClient("https://api.coincap.io/v2");
 
     // Get all coins
     public Coin[]? GetCoinMarket() => GetCoinMarket(100);
-    public Coin[]? GetCoinMarket(int pages)
+    public Coin[]? GetCoinMarket(int limit)
     {
-        var response = client.Execute(new RestRequest("/coins/markets", Method.Get).AddParameter("vs_currency", "usd").AddParameter("per_page", pages));
-        return response.IsSuccessful ? JsonConvert.DeserializeObject<Coin[]>(response.Content) : null;
+        var response = client.Execute(new RestRequest("/assets", Method.Get).AddParameter("limit", limit));
+        return response.IsSuccessful ? JObject.Parse(response.Content!)["data"]!.ToObject<Coin[]>() : null;
     }
 
     // Get the coin
-    public CoinFull? GetCoin(string CoinId)
+    public Coin? GetCoin(string coinId)
     {
-        var response = client.Execute(new RestRequest($"/coins/{CoinId}", Method.Get));
-        return response.IsSuccessful ? JsonConvert.DeserializeObject<CoinFull>(response.Content) : null;
+        var response = client.Execute(new RestRequest($"/assets/{coinId}", Method.Get));
+        return response.IsSuccessful ? JObject.Parse(response.Content!)["data"]!.ToObject<Coin>() : null;
+    }
+
+    // Dictionary of markets - market and price
+    public Dictionary<string, decimal>? GetMarkets(string coinId)
+    {
+        var response = client.Execute(new RestRequest($"/assets/{coinId}/markets", Method.Get));
+        return response.IsSuccessful ? JObject.Parse(response.Content!)["data"]!.DistinctBy(c => (string?)c["exchangeId"]).ToDictionary(c => (string?)c["exchangeId"], c => (decimal)c["priceUsd"]) : null;
     }
 
     // Exchange coins
-    public decimal? GetExchange(string TransferFrom, string TransferTo, decimal count)
+    public decimal? GetExchange(string from, string to, decimal count)
     {
-        var response = client.Execute(new RestRequest("/coins/markets", Method.Get).AddParameter("vs_currency", "usd").AddParameter("ids", $"{TransferFrom}, {TransferTo}"));
-        return response.IsSuccessful ? JArray.Parse(response.Content).Select(i => (decimal)i["current_price"]).Aggregate((x, y) => x / y * count) : null;
+        var response = client.Execute(new RestRequest("/assets", Method.Get).AddParameter("ids", $"{from},{to}"));
+        return response.IsSuccessful ? JObject.Parse(response.Content!)["data"]!.Select(i => (decimal?)i["priceUsd"]).Aggregate((x, y) => x / y * count) : null;
     }
 
     // Get the array of coins
     public string[]? GetCoinArray()
     {
-        var response = client.Execute(new RestRequest("/coins/list", Method.Get));
-        return response.IsSuccessful ? JArray.Parse(response.Content).Select(c => (string)c["id"]).ToArray() : null;
+        var response = client.Execute(new RestRequest("/assets", Method.Get).AddParameter("limit", 2000));
+        return response.IsSuccessful ? JObject.Parse(response.Content!)["data"]!.Select(c => (string?)c["id"]).ToArray() : null;
     }
 }
