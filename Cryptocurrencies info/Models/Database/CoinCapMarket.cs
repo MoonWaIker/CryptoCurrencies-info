ecd612be2eaba15de5c55376da4b23514d7c29e4
MoonWalker <muhinmihajlo40@gmail.com>
2023-08-15 11:23:00 +0300

    A little fix of models

--- Cryptocurrencies info/Models/Database/CoinCapMarket.cs
+++ Cryptocurrencies info/Models/Database/CoinCapMarket.cs
@@ -5,12 +5,12 @@ namespace Cryptocurrencies_info.Models.DataBase
     public class CoinCapMarket : MarketBase
     {
         [JsonProperty("exchangeId")]
-        public new string Name { get; set; } = string.Empty;
+        public override string Name { get; set; } = string.Empty;
         [JsonProperty("priceUsd")]
         public decimal Price { get; set; }
         [JsonProperty("baseSymbol")]
-        public new string Base { get; set; } = string.Empty;
+        public override string Base { get; set; } = string.Empty;
         [JsonProperty("quoteSymbol")]
-        public new string Target { get; set; } = string.Empty;
+        public override string Target { get; set; } = string.Empty;
     }
 }
--- Cryptocurrencies info/Models/Database/MarketBase.cs
+++ Cryptocurrencies info/Models/Database/MarketBase.cs
@@ -5,10 +5,10 @@ namespace Cryptocurrencies_info.Models.DataBase
     public class MarketBase
     {
         [MinLength(1)]
-        public string Name { get; set; } = string.Empty;
+        public virtual string Name { get; set; } = string.Empty;
         [MinLength(1)]
-        public string Base { get; set; } = string.Empty;
+        public virtual string Base { get; set; } = string.Empty;
         [MinLength(1)]
-        public string Target { get; set; } = string.Empty;
+        public virtual string Target { get; set; } = string.Empty;
     }
 }
